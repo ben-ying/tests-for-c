8526e6241fa7e9c8a2cc4fb02c4aedd23c2aaa64
ben <jiahi.ying@preh.cn>
2017-10-12 10:16:43 +0800

    add server

---
+++ websocket/Websocket-server/CMakeFiles/Websocket_server.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ben/app/tests-for-c/websocket/Websocket-server/tsd-websocket-mib3/1/workspace/tsd.websocket.mib3/app/tsd/websocket/mib3/WebsocketServer.cpp" "/home/ben/app/tests-for-c/websocket/Websocket-server/CMakeFiles/Websocket_server.dir/tsd-websocket-mib3/1/workspace/tsd.websocket.mib3/app/tsd/websocket/mib3/WebsocketServer.cpp.o"
+  "/home/ben/app/tests-for-c/websocket/Websocket-server/tsd-websocket-mib3/1/workspace/tsd.websocket.mib3/src/tsd/websocket/mib3/WebsocketTest.cpp" "/home/ben/app/tests-for-c/websocket/Websocket-server/CMakeFiles/Websocket_server.dir/tsd-websocket-mib3/1/workspace/tsd.websocket.mib3/src/tsd/websocket/mib3/WebsocketTest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "tsd-websocket-mib3/1/workspace/tsd.websocket.mib3/app/tsd/websocket/mib3"
+  "tsd-websocket-mib3/1/workspace/tsd.websocket.mib3/src/tsd/websocket/mib3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
